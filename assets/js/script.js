f55a1bb622ffca5307756d07a04b019bf68a1989
bleuEve <cathlauren09@gmail.com>
2023-12-09 12:45:18 +0700

    scalensip css & js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,31 +4,31 @@ let currentIndex = 0;
 let isAnimating = false;
 
 function showNextImage() {
-  if (!isAnimating) {
-    isAnimating = true;
-    setTimeout(() => {
-      imagesKevin[currentIndex].classList.remove('active');
-      currentIndex = (currentIndex + 1) % imagesKevin.length;
-      imagesKevin[currentIndex].classList.add('active');
-      isAnimating = false;
-    }, 100); // Adjust the delay (in milliseconds) as needed
-  }
+    if (!isAnimating) {
+        isAnimating = true;
+        setTimeout(() => {
+            imagesKevin[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % imagesKevin.length;
+            imagesKevin[currentIndex].classList.add('active');
+            isAnimating = false;
+        }, 100); // Adjust the delay (in milliseconds) as needed
+    }
 }
 
 // Function to check if an element is in the viewport
 function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= -300 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-  );
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= -300 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
 }
 
 // Function to handle scroll events
 function handleScroll() {
-  if (isInViewport(document.getElementById('image-sequence'))) {
-    showNextImage();
-  }
+    if (isInViewport(document.getElementById('image-sequence'))) {
+        showNextImage();
+    }
 }
 
 // Add scroll event listener
@@ -39,21 +39,21 @@ handleScroll();
 
 /*ANIMATION-2*/
 const images = document.querySelectorAll('.photos img');
-            
-            function changePosition() {
-                // Menyimpan referensi gambar terakhir
-                const lastImageClass = images[0].className;
 
-                // Menggeser kelas setiap gambar
-                for (let i = 0; i < images.length - 1; i++) {
-                    images[i].className = images[i + 1].className;
-                }
+function changePosition() {
+    // Menyimpan referensi gambar terakhir
+    const lastImageClass = images[0].className;
 
-                // Memindahkan gambar pertama ke posisi terakhir
-                images[images.length - 1].className = lastImageClass;
-            }
+    // Menggeser kelas setiap gambar
+    for (let i = 0; i < images.length - 1; i++) {
+        images[i].className = images[i + 1].className;
+    }
 
-            setInterval(changePosition, 2000);
+    // Memindahkan gambar pertama ke posisi terakhir
+    images[images.length - 1].className = lastImageClass;
+}
+
+setInterval(changePosition, 2000);
 
 /*ANIMATION-3*/
 document.addEventListener('DOMContentLoaded', function () {
@@ -88,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
             triggerHook: 0.7, // Set triggerHook based on your preference
             duration: '100%', // Set the duration as needed
         })
-            .setTween(gsap.fromTo(desc, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 },{ opacity: 1, y: 0, duration: 0.6 }))
+            .setTween(gsap.fromTo(desc, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 }, { opacity: 1, y: 0, duration: 0.6 }))
             .addTo(controller);
     }
 
@@ -110,83 +110,105 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 /*PRODUCT-COLLABS*/
-    function hoverd100(img){
-        img.src = "assets/image/disney100logo.png"}
+function hoverd100(img) {
+    img.src = "assets/image/disney100logo.png"
+}
 
-    function hoverOutd100(img){
-        img.src = "assets/image/collabdisney100.png"}
+function hoverOutd100(img) {
+    img.src = "assets/image/collabdisney100.png"
+}
 
-    function hoverhp(img){
-        img.src = "assets/image/hplogo.png"}
+function hoverhp(img) {
+    img.src = "assets/image/hplogo.png"
+}
 
-    function hoverOuthp(img){
-        img.src = "assets/image/collabharrypotter.png"}
+function hoverOuthp(img) {
+    img.src = "assets/image/collabharrypotter.png"
+}
 
-    function hoversw(img){
-        img.src = "assets/image/starwarslogo.png"}
-        
-    function hoverOutsw(img){
-        img.src = "assets/image/collabstarwars.png"}
+function hoversw(img) {
+    img.src = "assets/image/starwarslogo.png"
+}
 
-    function hovermarvel(img){
-        img.src = "assets/image/marvellogo.png"}
-        
-    function hoverOutmarvel(img){
-        img.src = "assets/image/collabmarvel.png"}
-    
-    function hoverprincess(img){
-        img.src = "assets/image/disneyprincesslogo.png"}
-        
-    function hoverOutprincess(img){
-        img.src = "assets/image/collabdineyprincess.png"}
+function hoverOutsw(img) {
+    img.src = "assets/image/collabstarwars.png"
+}
 
-    function hoverdisney(img){
-        img.src = "assets/image/disneylogo.png"}
-        
-    function hoverOutdisney(img){
-        img.src = "assets/image/collabdisney.png"}
+function hovermarvel(img) {
+    img.src = "assets/image/marvellogo.png"
+}
 
-    function hoverrifle(img){
-        img.src = "assets/image/riflepaperlogo.png"}
-        
-    function hoverOutrifle(img){
-        img.src = "assets/image/collabrifle.png"}
+function hoverOutmarvel(img) {
+    img.src = "assets/image/collabmarvel.png"
+}
 
-    function hovergray(img){
-        img.src = "assets/image/graymalinlogo.png"}
-            
-    function hoverOutgray(img){
-        img.src = "assets/image/collabgraymalin.png"}
+function hoverprincess(img) {
+    img.src = "assets/image/disneyprincesslogo.png"
+}
 
-    function hovervineyard(img){
-        img.src = "assets/image/vineyardlogo.png"}
-                
-    function hoverOutvineyard(img){
-        img.src = "assets/image/collabvineyard.png"}
+function hoverOutprincess(img) {
+    img.src = "assets/image/collabdineyprincess.png"
+}
 
-    function hoverstance(img){
-        img.src = "assets/image/stancelogo.png"}
-                    
-    function hoverOutstance(img){
-        img.src = "assets/image/collabstance.png"}
+function hoverdisney(img) {
+    img.src = "assets/image/disneylogo.png"
+}
 
-    function hovermatrix(img){
-        img.src = "assets/image/matrixlogo.png"}
-                        
-    function hoverOutmatrix(img){
-        img.src = "assets/image/collabthematrix.png"}
+function hoverOutdisney(img) {
+    img.src = "assets/image/collabdisney.png"
+}
+
+function hoverrifle(img) {
+    img.src = "assets/image/riflepaperlogo.png"
+}
+
+function hoverOutrifle(img) {
+    img.src = "assets/image/collabrifle.png"
+}
+
+function hovergray(img) {
+    img.src = "assets/image/graymalinlogo.png"
+}
+
+function hoverOutgray(img) {
+    img.src = "assets/image/collabgraymalin.png"
+}
+
+function hovervineyard(img) {
+    img.src = "assets/image/vineyardlogo.png"
+}
+
+function hoverOutvineyard(img) {
+    img.src = "assets/image/collabvineyard.png"
+}
+
+function hoverstance(img) {
+    img.src = "assets/image/stancelogo.png"
+}
+
+function hoverOutstance(img) {
+    img.src = "assets/image/collabstance.png"
+}
+
+function hovermatrix(img) {
+    img.src = "assets/image/matrixlogo.png"
+}
+
+function hoverOutmatrix(img) {
+    img.src = "assets/image/collabthematrix.png"
+}
 
 
 /* Animation 5 */
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let mainCircle = document.getElementById("mainCircle");
     let smallCircles = document.querySelectorAll(".circle");
 
-    mainCircle.addEventListener("click", function() {
+    mainCircle.addEventListener("click", function () {
         mainCircle.classList.add("active");
 
-        setTimeout(function() {
-            smallCircles.forEach(function(circle) {
+        setTimeout(function () {
+            smallCircles.forEach(function (circle) {
                 circle.classList.add("active");
             });
             mainCircle.style.opacity = 0;
@@ -201,7 +223,7 @@ function changeContentColorInteractive(imageSrc, newText, element) {
     document.getElementById('text').className = 'new-text-style';
 
     var gridItems = document.querySelectorAll('.grid-item-color-interactive');
-    gridItems.forEach(function(item) {
+    gridItems.forEach(function (item) {
         // Hanya menghilangkan kelas 'active' atau kelas yang menandai elemen aktif
         item.classList.remove('grid-item-active'); // 'active' adalah contoh, ganti dengan kelas yang sesuai
     });
@@ -211,7 +233,7 @@ function changeContentColorInteractive(imageSrc, newText, element) {
 }
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Pilih elemen grid yang ingin diaktifkan secara otomatis
     var gridItemToActivate = document.querySelector('.grid-item-color-interactive:nth-child(1)'); // Ganti 'nth-child(1)' dengan urutan grid yang benar
 
@@ -220,3 +242,168 @@ document.addEventListener('DOMContentLoaded', function() {
         gridItemToActivate.click();
     }
 });
+
+/* SCALE & SIP-BANNER */
+const banner = document.querySelector('.scaleNsip-banner');
+const img = banner.querySelector('img');
+const video = banner.querySelector('video');
+
+banner.addEventListener('mouseover', () => {
+    img.style.opacity = '0'; // Hide the image
+    video.play(); // Start playing the video
+});
+
+banner.addEventListener('mouseout', () => {
+    img.style.opacity = '1'; // Show the image on mouseout
+    video.pause(); // Pause the video
+    video.currentTime = 0; // Reset video to the beginning
+});
+
+/* SCALE & SIP-INTERACTIVE */
+// JavaScript function to update subcategory dropdown based on the selected category
+function updateSubcategories(setNumber) {
+    var categoryDropdown = document.getElementById("categoryDropdown" + setNumber);
+    var subcategoryDropdown = document.getElementById("subcategoryDropdown" + setNumber);
+
+    // Get the selected category and subcategory
+    var selectedCategory = categoryDropdown.options[categoryDropdown.selectedIndex].value;
+
+    // Clear existing options in subcategory dropdown
+    subcategoryDropdown.innerHTML = '';
+
+    // Populate subcategory dropdown based on the selected category
+    switch (selectedCategory) {
+        case "category1":
+            addOption(subcategoryDropdown, "subcat1-1", "16 oz");
+            break;
+        case "category2":
+            addOption(subcategoryDropdown, "subcat2-1", "16 oz");
+            addOption(subcategoryDropdown, "subcat2-2", "20 oz");
+            addOption(subcategoryDropdown, "subcat2-3", "40 oz");
+            break;
+        case "category3":
+            addOption(subcategoryDropdown, "subcat3-1", "16 oz");
+            addOption(subcategoryDropdown, "subcat3-2", "20 oz");
+            break;
+        case "category4":
+            addOption(subcategoryDropdown, "subcat4-1", "16 oz");
+            break;
+        case "category5":
+            addOption(subcategoryDropdown, "subcat5-1", "20 oz");
+            break;
+        case "category6":
+            addOption(subcategoryDropdown, "subcat6-1", "16 oz");
+            addOption(subcategoryDropdown, "subcat6-2", "20 oz");
+            addOption(subcategoryDropdown, "subcat6-3", "25 oz");
+            break;
+        case "category7":
+            addOption(subcategoryDropdown, "subcat7-1", "20 oz");
+            break;
+        case "category8":
+            addOption(subcategoryDropdown, "subcat8-1", "20 oz");
+            addOption(subcategoryDropdown, "subcat8-2", "32 oz");
+            break;
+        case "category9":
+            addOption(subcategoryDropdown, "subcat9-1", "16 oz");
+            addOption(subcategoryDropdown, "subcat9-2", "20 oz");
+            addOption(subcategoryDropdown, "subcat9-3", "24 oz");
+            addOption(subcategoryDropdown, "subcat9-4", "25 oz");
+            break;
+        case "category10":
+            addOption(subcategoryDropdown, "subcat10-1", "16 oz");
+            addOption(subcategoryDropdown, "subcat10-2", "20 oz");
+            break;
+
+        // Add cases for other categories and their respective subcategories
+    }
+    updateResultImage(setNumber);
+}
+
+// Helper function to add options to a dropdown
+function addOption(dropdown, value, text) {
+    var option = document.createElement("option");
+    option.value = value;
+    option.text = text;
+    dropdown.add(option);
+}
+
+// Additional functions for updating the result image
+function getImagePath(subcategory) {
+
+    // Customize this function based on your image naming convention and folder structure
+    return "assets/image/" + subcategory + ".png";
+}
+
+function updateResultImage(setNumber) {
+    var subcategoryDropdown = document.getElementById("subcategoryDropdown" + setNumber);
+    var resultImage = document.getElementById("resultImage" + setNumber);
+
+    var selectedSubcategory = subcategoryDropdown.options[subcategoryDropdown.selectedIndex].value;
+
+    var imagePath = 'assets/image/subcat1-1.png';
+
+
+    if (selectedSubcategory === "subcat1-1") {
+        imagePath = 'assets/image/subcat1-1.png';
+    }
+    else if (selectedSubcategory === "subcat2-1") {
+        imagePath = 'assets/image/subcat2-1.png';
+    }
+    else if (selectedSubcategory === "subcat2-2") {
+        imagePath = 'assets/image/subcat2-2.png';
+    }
+    else if (selectedSubcategory === "subcat2-3") {
+        imagePath = 'assets/image/subcat2-3.png';
+    }
+    else if (selectedSubcategory === "subcat3-1") {
+        imagePath = 'assets/image/subcat3-1.png';
+    }
+    else if (selectedSubcategory === "subcat3-2") {
+        imagePath = 'assets/image/subcat3-2.png';
+    }
+    else if (selectedSubcategory === "subcat4-1") {
+        imagePath = 'assets/image/subcat4-1.png';
+    }
+    else if (selectedSubcategory === "subcat5-1") {
+        imagePath = 'assets/image/subcat5-1.png';
+    }
+    else if (selectedSubcategory === "subcat6-1") {
+        imagePath = 'assets/image/subcat6-1.png';
+    }
+    else if (selectedSubcategory === "subcat6-2") {
+        imagePath = 'assets/image/subcat6-2.png';
+    }
+    else if (selectedSubcategory === "subcat6-3") {
+        imagePath = 'assets/image/subcat6-3.png';
+    }
+    else if (selectedSubcategory === "subcat7-1") {
+        imagePath = 'assets/image/subcat7-1.png';
+    }
+    else if (selectedSubcategory === "subcat8-1") {
+        imagePath = 'assets/image/subcat8-1.png';
+    }
+    else if (selectedSubcategory === "subcat8-2") {
+        imagePath = 'assets/image/subcat8-2.png';
+    }
+    else if (selectedSubcategory === "subcat9-1") {
+        imagePath = 'assets/image/subcat9-1.png';
+    }
+    else if (selectedSubcategory === "subcat9-2") {
+        imagePath = 'assets/image/subcat9-2.png';
+    }
+    else if (selectedSubcategory === "subcat9-3") {
+        imagePath = 'assets/image/subcat9-3.png';
+    }
+    else if (selectedSubcategory === "subcat9-4") {
+        imagePath = 'assets/image/subcat9-4.png';
+    }
+    else if (selectedSubcategory === "subcat10-1") {
+        imagePath = 'assets/image/subcat10-1.png';
+    }
+    else if (selectedSubcategory === "subcat10-2") {
+        imagePath = 'assets/image/subcat10-2.png';
+    }
+
+    resultImage.innerHTML = '<img src="' + imagePath + '" alt= "Selected Image" style="max-width: 100%; height: auto;">';
+
+}
